1dd1851e9e3910b6d25c7d997569b63731e33e02
Haritha Madana <60546642+harithamadana@users.noreply.github.com>
2022-04-22 09:13:22 +0530

    Update Edit_Price.java

--- src/main/Edit_Price.java
+++ src/main/Edit_Price.java
@@ -11,10 +11,7 @@ import java.sql.ResultSet;
 import javax.swing.JOptionPane;
 import net.proteanit.sql.DbUtils;
 
-/**
- *
- * @author Hemaa
- */
+
 public class Edit_Price extends javax.swing.JFrame {
 Connection con = null;
 PreparedStatement pre = null;
